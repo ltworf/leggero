780221e9a2d96bd4de19e6e2f7a2991fe7d7e35f
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2019-12-27 10:35:09 +0100

    Fix copyright notice

--- src/lilypondrunner.h
+++ src/lilypondrunner.h
@@ -12,7 +12,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Trabucco.  If not, see <http://www.gnu.org/licenses/>.
+along with Leggero.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (C) 2019  Salvo "LtWorf" Tomaselli
 */
